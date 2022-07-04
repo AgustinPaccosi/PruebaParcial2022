57293a47d3fd8bcbd40c22dd2712b8df5e330907
AgustinPaccosi <Aguspaccosi@hotmail.com>
2022-07-04 19:03:25 -0300

    Ultimo cabio, Actualice y le agregue el 0 donde faltaba.

--- Program.cs
+++ Program.cs
@@ -16,17 +16,20 @@ namespace PruebaParcial2022
                 Console.WriteLine("Ingrese los Segundos");
                 var segundos = int.Parse(Console.ReadLine());
                 Console.WriteLine("Ingrese si corresponden a (PM) o (AM) ");
-                var parte = ((Console.ReadLine().ToUpper)());
+                var parte = (Console.ReadLine().ToUpper());
                 if (Horario24(parte) && Verifica(horas, minutos, segundos))
                 {
+                    Console.WriteLine($"Se muestra el Horario formato 12: {Verificanum(horas,minutos,segundos)}  {parte}");
                     horas = horas + 12;
-                    Console.WriteLine($"Se muestra el Horario {horas}:{minutos}:{segundos} {parte}");
+                    //Console.WriteLine($"Se muestra el Horario formato 24: {horas}:{minutos}:{segundos} {parte}");
+                    Console.WriteLine($"El horario Formato 24 es tambien: {Verificanum(horas,minutos,segundos)}  ");
                 }
                 else
                 {
-                    if (Verifica(horas, minutos, segundos) && Horario24(parte)==false)
+                    if (Verifica(horas, minutos, segundos))
                     {
-                        Console.WriteLine($"Se muestra el Horario {horas}:{minutos}:{segundos} {parte}");
+                        Console.WriteLine($"Se muestra el Horario formato 12: {Verificanum(horas,minutos,segundos)} {parte}");
+                        Console.WriteLine($"Se muestra el Horario formato 24: {Verificanum(horas,minutos,segundos)} ");
                     }
                     else
                     {
@@ -53,7 +56,7 @@ namespace PruebaParcial2022
         }
         private static bool Verifica(int horas, int minutos, int segundos)
         {
-            if ((horas > 0) && (horas <= 12) && (minutos > 0) && (minutos <= 60) && (segundos > 0) && (segundos <= 60))
+            if ((horas >= 0) && (horas <= 12) && (minutos >= 0) && (minutos < 60) && (segundos >= 0) && (segundos < 60))
             {
                 return true;
             }
@@ -61,6 +64,15 @@ namespace PruebaParcial2022
             {
                 return false;
             }
+        }
+        private static string Verificanum(int horas, int minutos, int segundos)
+        {
+            //if (horas<10 || minutos<10 || segundos<10)
+            //{
+                var msj = horas.ToString().PadLeft(2, '0')+":"+ minutos.ToString().PadLeft(2, '0') +":"+ segundos.ToString().PadLeft(2, '0');
+                return msj;
+            //}
+
         }
 
     }
